7f7730ce3b2ee8c2e0ef347004d172f080a416d4
Rehab-Samy18 <60798015+Rehab-Samy18@users.noreply.github.com>
2022-06-24 16:54:23 +0200

    Time: 214 ms (17.51%), Space: 41.6 MB (58.75%) - LeetHub

--- 704-binary-search/704-binary-search.cs
+++ 704-binary-search/704-binary-search.cs
@@ -1,13 +1,5 @@
 public class Solution {
     public int Search(int[] nums, int target) {
-        int res=-1;
-        for(int i = 0;i < nums.Length; i++)
-        {
-            if(nums[i] == target)
-            {
-                res = i;
-            }
-        }
-        return res;
+        return nums.Contains(target)?Array.IndexOf(nums,target):-1;
     }
 }
